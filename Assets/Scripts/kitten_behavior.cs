eb749383d6a003ad046db62dc7e8e6749fce8fbe
chrysothemis <sonya.utecht@gmail.com>
2014-08-01 16:27:55 -0500

    Kittens spawn!

--- Assets/Scripts/kitten_behavior.cs
+++ Assets/Scripts/kitten_behavior.cs
@@ -5,12 +5,15 @@ public class kitten_behavior : MonoBehaviour {
 	int time_til_slide;
 	public Animator anim;
 	public Rigidbody2D my_body;
+	public bool grabbed;
+	float grav;
 
 	// Use this for initialization
 	void Start () {
 		anim.SetBool("idling", true);
-		time_til_slide = Random.Range(2,30);
-		Debug.Log(time_til_slide);
+		grabbed = false;
+		time_til_slide = Random.Range(1,10);
+		//Debug.Log(time_til_slide);
 		StartCoroutine(prepare_to_slide());
 	}
 	
@@ -23,6 +26,30 @@ public class kitten_behavior : MonoBehaviour {
 		yield return new WaitForSeconds(time_til_slide);
 		Debug.Log("Sliding!");
 		anim.SetBool("idling", false);
-		my_body.gravityScale = Random.Range(.05f,.5f);
+		grav = Random.Range(.05f,.5f);
+		my_body.gravityScale = grav;
 	}
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		// If the colliding gameobject is bad...
+		if(col.gameObject.tag == "Bad"){
+			Debug.Log("Kitten Missed!");
+			//damage player
+			gameObject.GetComponent<SpriteRenderer>().enabled = false;
+			gameObject.SetActive(false);
+			//AudioSource.PlayClipAtPoint(boss_damage1, transform.position);
+		}
+
+		//if(col.gameObject.tag == "player"){
+		//	Debug.Log("Kitten Grabbed!");
+		//	//damage player
+		//	holder = col.gameObject;
+		//	my_body.gravityScale = 0;
+			//gameObject.SetActive(false);
+			//AudioSource.PlayClipAtPoint(boss_damage1, transform.position);
+		//}
+
+	}
+
 }
---
+++ Assets/Scripts/kitten_spawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kitten_spawner : MonoBehaviour {
+	int min;
+	int max; //make spawn rate adjustable
+	public GameObject kittenModel;
+
+	// Use this for initialization
+	void Start () {
+		min = 1;
+		max = 3;	
+		StartCoroutine(spread_cats());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator spread_cats(){
+		yield return new WaitForSeconds(Random.Range(min, max));
+		GameObject kitten = (GameObject) Instantiate(kittenModel, new Vector3( Random.Range(1.7f, 14.7f), 0.6f, 0) , transform.rotation); //spawn the kitten on a random x value at the top of the slide
+		StartCoroutine(spread_cats());
+	}
+}
